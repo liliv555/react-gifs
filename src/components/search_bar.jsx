f53225d643cf3ee5928cdf2b3f88d79468f1b7be
lili vincze <lili.vincze@gmail.com>
2021-11-12 11:17:10 +0100

    search bar focus listener added

--- src/components/search_bar.jsx
+++ src/components/search_bar.jsx
@@ -1,17 +1,31 @@
 import React, { Component } from 'react';
 
 class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      focused: false
+    };
+  }
+
   handleUpdate = (event) => {
     this.props.searchFunction(event.target.value);
   };
 
+  focus = () => {
+    this.setState({ focused: true });
+  }
+
   render() {
     return (
       <div>
-        <i className="fas fa-search"></i>
+        <i className="fas fa-search" />
         <input
+          focused={this.state.focused}
           type="text"
           className="form-control form-search"
+          onFocus={this.focus}
           onChange={this.handleUpdate}
         />
       </div>
